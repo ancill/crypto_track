b3c46660ecefb3f1718a21413a91517ae3b6b92d
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2022-10-17 20:01:48 +0500

    Create api.js

---
+++ src/api.js
@@ -0,0 +1,9 @@
+const API_KEY =
+  "f4829e03fd35e85421baf7d5b747d5ccaf6ba31428a825af81659e9f02fb8777"
+
+//refactor to  URLsearchParams
+export const loadTicker = (tickerName) => {
+  fetch(
+    `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=${API_KEY}`
+  ).then((r) => r.json())
+}
